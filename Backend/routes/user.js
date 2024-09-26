997c95217a6ecccf9552037bcc80fff27a9bbb46
Sambuddha Biswas <38996553+XiLucas@users.noreply.github.com>
2024-09-26 16:25:18 +0530

    adding try catch

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,9 +1,22 @@
 const { Router } = require("express");
+const { userModel } = require("../db");
 const userRouter = Router();
 
-userRouter.post("/signup", function (req, res) {
+userRouter.post("/signup", async function (req, res) {
+  const { email, password, firstName, lastName } = req.body;
+  //const email =req.body.email;(we used to do like this)
+  try {
+    await userModel.create({
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastname: lastName,
+    });
+  } catch (error) {
+    console.error("Error occured:", error.message);
+  }
   res.json({
-    message: "signup endpoint",
+    message: "signup succeded",
   });
 });
 
